07f8d1347f30a54aaa9a720438a7465743011d0c
Samuel Fletcher <49329336+SamuelFlet@users.noreply.github.com>
2022-04-07 16:53:51 -0400

    Added Resume

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,6 +4,7 @@ import "../styles/App.css";
 import weatherapp from "../assets/weatherapp.png";
 import unsent from "../assets/unsent.png";
 import headphone from "../assets/headphone.png";
+import resume from "../assets/resume.pdf";
 
 export default function App() {
   return (
@@ -15,12 +16,14 @@ export default function App() {
           with React, Vue, and multiple backend frameworks
         </p>
         <div className="pt-5">
-          <button
-            type="button"
-            className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-10 py-2.5 text-center mr-2 mb-2"
-          >
-            My Resume
-          </button>
+          <a href={resume}>
+            <button
+              type="button"
+              className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-10 py-2.5 text-center mr-2 mb-2"
+            >
+              My Resume
+            </button>
+          </a>
         </div>
       </div>
       <div className="w-24 bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl font-medium rounded-md px-1 py-1.5 mr-1 mb-2" />
